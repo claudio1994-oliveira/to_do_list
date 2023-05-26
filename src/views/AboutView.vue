d6e4f3ef1351b71e2c7c75b3b62db8c922641755
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-05-25 21:24:07 -0300

    Alterando texto do AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Minhas tarefas</h1>
+    <p>
+      Aplicação para testar meus conhecimentos no Vue Js e para organizar minhas tarefas.
+    </p>
   </div>
 </template>
