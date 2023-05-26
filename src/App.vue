0c6db172f23fbfff318849a435541a9ad56d1eda
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-05-25 21:24:31 -0300

    Alterando nav do menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
     <router-link :to="{name: 'todos.index'}">Todos</router-link>
+    <router-link to="/about">Sobre</router-link>
   </nav>
   <router-view/>
 </template>
